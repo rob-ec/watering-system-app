6f34fc8dec06471ccfb834abe5e21b4007fe61c1
Robson Mesquita Gomes <robson.mesquita56@gmail.com>
2026-02-19 21:48:42 -0300

    💄 style(DeviceWifi): Improove DeviceWiFi UX

--- components/Device/DeviceWiFi.tsx
+++ components/Device/DeviceWiFi.tsx
@@ -1,21 +1,57 @@
+import color from "tailwindcss/colors";
 import { Wifi } from "@/types/wifi";
+import { Globe, GlobeOff, HouseWifi, WifiOff } from "lucide-react-native";
 import { Text, View } from "react-native";
 
 interface DeviceWiFiProps {
     wifi: Wifi;
 }
 
-export const DeviceWiFi = ({wifi}: DeviceWiFiProps) => {
+export const DeviceWiFi = ({ wifi }: DeviceWiFiProps) => {
+    const { hasInternetConnection } = wifi;
+    const hasWifiConnection = true; // for future BLE connections
+
     return (
         <View className={styles.container}>
-            <Text className={styles.label}>WiFi</Text>
-            <Text className={styles.status}>{wifi.hasInternetConnection ? "Conectado" : "Desconectado"}</Text>
+            <Text className={styles.label}>Conectividade</Text>
+            <View className={styles.section}>
+                <View className={styles.connection.container}>
+                    {
+                        hasWifiConnection ?
+                            <HouseWifi size={32} stroke={color.indigo[500]} />
+                            :
+                            <WifiOff size={32} stroke={color.gray[500]} />
+                    }
+                    <Text className={styles.connection.value}>
+                        {hasWifiConnection ? "Conectado" : "Desconectado"}
+                    </Text>
+                    <Text className={styles.connection.label}>Rede local</Text>
+                </View>
+                <View className={styles.connection.container}>
+                    {
+                        hasInternetConnection ?
+                            <Globe size={32} stroke={color.teal[500]} />
+                            :
+                            <GlobeOff size={32} stroke={color.gray[500]} />
+                    }
+                    <Text className={styles.connection.value}>
+                        {hasInternetConnection ? "Conectado" : "Desconectado"}
+                    </Text>
+                    <Text className={styles.connection.label}>Internet</Text>
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = {
-    container: `flex flex-col w-full`,
-    label: `text-sm`,
+    container: `flex flex-col w-full p-3`,
+    label: `text-xs mb-4`,
+    section: `flex flex-row gap-2`,
     status: `text-xl font-bold`,
+    connection: {
+        container: `flex flex-col gap-2 flex-1`,
+        label: `text-sm`,
+        value: `text-xl font-bold`,
+    }
 };
